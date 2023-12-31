1edfdacd1c5add13a0acb9b7533637f286e67abe
Sukhendu Sain <83855289+ssain2021@users.noreply.github.com>
2023-12-31 14:31:15 +0530

    Delete websites directory

--- websites/website/Common Backend/token-presale-details.js
+++
@@ -1,7 +0,0 @@
-var tokensSoldinStage = 0;
-var currentStage = 0;
-var previousStages = new Array();
-var uncompletedStages = new Array();
-var tokensLeftinStage = 0;
-var USDTraised = 0;
-var totalUnsoldTokens = 100000;
